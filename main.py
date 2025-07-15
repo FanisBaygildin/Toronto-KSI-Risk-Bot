1f7e98a3f98df3d9526abc368df35abffa48849a
FanisBaygildin <134169743+FanisBaygildin@users.noreply.github.com>
2025-07-15 19:21:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@
 # Render knows it needs to start this file
 from telegram_bot0 import start
 
-if __name__ == "__main__":
+if __name__ == "__main__":    # if we launch the file directly
     start()
