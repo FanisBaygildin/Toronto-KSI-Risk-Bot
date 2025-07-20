517e370c0cbecd3098bdc4fbcba5ca708e936fd3
FanisBaygildin <134169743+FanisBaygildin@users.noreply.github.com>
2025-07-20 13:37:39 -0400

    Update telegram_bot0.py

--- telegram_bot0.py
+++ telegram_bot0.py
@@ -51,8 +51,7 @@ async def receive_end_pc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     
     # готовим подпись
     caption_lines = [
-        f"Current Weather: {weather_str}" + \
-        [
+        f"Current Weather: {weather_str}" + [
         f"Route {i+1}: {r['distance_km']} km, {r['duration_text']} "
         f"[{', '.join(r['geohash5'])}]"
         for i, r in enumerate(routes)
